edfb637255a03dedb58db2819a20d51d8e0db83e
Mohammed Sadik <msadikjowel@gmail.com>
2023-02-07 23:06:50 +0600

    added dark mode

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,18 @@
 import Header from "@/components/Header";
 import "./globals.css";
+import Providers from "./Providers";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        {/* header */}
-        <Header />
-        {/* navbar */}
-        {/* search box */}
-        {children}
+        <Providers>
+          {/* header */}
+          <Header />
+          {/* navbar */}
+          {/* search box */}
+          {children}
+        </Providers>
       </body>
     </html>
   );
