3aca5771234b789afaae679161abb5837156a7f2
Mohammed Sadik <msadikjowel@gmail.com>
2023-02-08 12:29:51 +0600

    created navbar component

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,6 +1,6 @@
 export default function page() {
   return (
-    <div className="max-w-6xl mx-auto space-y-4">
+    <div className="max-w-6xl mx-auto space-y-4 p-4">
       <h1 className="text-2xl font-med text-amber-600">About</h1>
       <p>
         Welcome to our movie database website! We are a team of passionate movie
@@ -10,11 +10,16 @@ export default function page() {
         the latest news, reviews and trailers.
       </p>
       <p>
-        Welcome to our movie database website! We are a team of passionate movie
-        enthusiasts who have come together to create a one-stop destination for
-        all you movie-related needs. Our website is designed to provide you with
-        a comprehensive database of movies from all around the world, along with
-        the latest news, reviews and trailers.
+        A movie database website is a comprehensive online platform that
+        provides users with a vast array of information related to movies. This
+        includes movie details such as cast, crew, plot synopsis, release dates,
+        ratings, reviews, and trailers. The website also provides users with the
+        ability to search and filter movies based on various criteria such as
+        genre, language, and release year. Additionally, users can create their
+        own lists, rate and review movies, and participate in forums and
+        discussions. The ultimate goal of the movie database website is to offer
+        a one-stop destination for movie enthusiasts to discover, learn, and
+        share their passion for film.
       </p>
     </div>
   );
--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,4 +1,5 @@
 import Header from "@/components/Header";
+import Navbar from "@/components/Navbar";
 import "./globals.css";
 import Providers from "./Providers";
 
@@ -10,6 +11,7 @@ export default function RootLayout({ children }) {
           {/* header */}
           <Header />
           {/* navbar */}
+          <Navbar />
           {/* search box */}
           {children}
         </Providers>
