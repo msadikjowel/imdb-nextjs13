b07f11da1eadbcbd026bd356dfc09d3f49304369
Mohammed Sadik <msadikjowel@gmail.com>
2023-02-08 10:56:03 +0600

    created about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,21 @@
+export default function page() {
+  return (
+    <div className="max-w-6xl mx-auto space-y-4">
+      <h1 className="text-2xl font-med text-amber-600">About</h1>
+      <p>
+        Welcome to our movie database website! We are a team of passionate movie
+        enthusiasts who have come together to create a one-stop destination for
+        all you movie-related needs. Our website is designed to provide you with
+        a comprehensive database of movies from all around the world, along with
+        the latest news, reviews and trailers.
+      </p>
+      <p>
+        Welcome to our movie database website! We are a team of passionate movie
+        enthusiasts who have come together to create a one-stop destination for
+        all you movie-related needs. Our website is designed to provide you with
+        a comprehensive database of movies from all around the world, along with
+        the latest news, reviews and trailers.
+      </p>
+    </div>
+  );
+}
